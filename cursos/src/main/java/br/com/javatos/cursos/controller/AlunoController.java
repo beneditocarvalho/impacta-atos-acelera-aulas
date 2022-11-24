bbb5bfca453ce193025e251f883e6b29add27ed0
Benedito Carvalho <benedito.carvalho@gmail.com>
2022-11-23 23:30:09 -0300

    Java Web Aula 5 (Parte 6) - Adicionando o Swagger

--- cursos/src/main/java/br/com/javatos/cursos/controller/AlunoController.java
+++ cursos/src/main/java/br/com/javatos/cursos/controller/AlunoController.java
@@ -2,34 +2,45 @@ package br.com.javatos.cursos.controller;
 
 import br.com.javatos.cursos.model.Aluno;
 import br.com.javatos.cursos.service.AlunoService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/aluno")
 public class AlunoController {
-
-    @Autowired
     private final AlunoService alunoService;
 
     @PostMapping
-    public Aluno salvar(@RequestBody Aluno aluno){
-
+    @ApiOperation(value = "salvar uma aluno")
+    public Aluno salvar(@RequestBody Aluno aluno) {
         return alunoService.salvar(aluno);
-
     }
 
     @GetMapping
-    public List<Aluno> listarTodosS( ) {
+    @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation(value = "Listar todos os alunos")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "sucesso"),
+            @ApiResponse(code = 201, message = "criado"),
+            @ApiResponse(code = 401, message = "vc não tem autorização"),
+            @ApiResponse(code = 403, message = "vc não pode ver o que tem aqui"),
+            @ApiResponse(code = 404, message = "não tem nada aqui"),
+            @ApiResponse(code = 500, message = "falha do programador")
+    })
+    public List<Aluno> listarTodos() {
         return alunoService.listarTodos();
     }
 
     @PutMapping
-    public Aluno alterar(Aluno aluno){
-        return alunoService.alterar(aluno);
+    @ApiOperation(value = "Alterar os dados de um Aluno")
+    public Aluno alterar (@RequestBody Aluno aluno){
+       return alunoService.alterar(aluno);
+
     }
 }
--- cursos/src/main/java/br/com/javatos/cursos/model/Aluno.java
+++ cursos/src/main/java/br/com/javatos/cursos/model/Aluno.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -20,6 +21,6 @@ public class Aluno {
     private String email;
     private String matricula;
     private Endereco endereco;
-    private List<Curso> cursos;
-    private List<Nota> notas;
+    private List<Curso> cursos = new ArrayList<>();
+    private List<Nota> notas = new ArrayList<>();
 }
